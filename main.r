69b6215fc38d2bd53a1f70808de72a864ffd5b3b
Bibek Itani <43322077+bibekitani34@users.noreply.github.com>
2022-02-21 20:34:18 +0000

    Initial commit

---
+++ main.r
@@ -0,0 +1,16 @@
+#Author: Bibek Itani, Date: Feb 21, 2022, Purpose: Calculate stdev, Median values.
+
+# Calculate standard deviation and standard deviation. 
+
+# A variable to hold 7 numbers.
+var1 <- c(1, 3, 5, 7, 9, 11, 13)
+
+std_dev <- sd(var1)
+print(paste("The standard deviation of the given list is:", std_dev ))
+# 4.320494
+
+# Calculate median. 
+med <- median(var1)
+print(paste("The median of the given list is:", med))
+#7
+
